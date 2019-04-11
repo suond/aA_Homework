5bc5e87b17af3880abdea3a0267c53c9f84f233b
david <dsuon04@gmail.com>
2019-04-10 19:30:20 -0700

    rails auth

--- week9/day4/rails_auth/app/models/user.rb
+++ week9/day4/rails_auth/app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
     validates :session_token, presence: true, uniqueness: true
     validates :password_digest, presence: {message: "Password can't be blank"}
     validates :password, length: {minimum: 6, allow_nil: true}
+    before_validation :ensure_session_token
 
     attr_reader :password
 
