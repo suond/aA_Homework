2113a94eec52345669a4f63a6cf9390dfd3be311
david <dsuon04@gmail.com>
2019-04-11 19:18:20 -0700

    user mailer

---
+++ week9/day5/solution/app/mailers/user_mailer.rb
@@ -0,0 +1,12 @@
+class UserMailer < ApplicationMailer
+    default from: 'from@example.com'
+
+    def welcome_email(user)
+        @user = user
+        @url = 'http://example.com/login'
+        mail(to: user.username, subject: "Welcome to my awesome site")
+    end
+
+
+
+end 
